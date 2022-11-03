e9edfa3b14c33aa85810a0d4b227bca446acfe8e
William Lucas <williamsantos1506@gmail.com>
2022-11-03 19:41:02 -0300

    first commit

---
+++ questions.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    question: "Quanto é 1 + 2?",
+    answers: [
+      { option: "3", correct: true },
+      { option: "2", correct: false },
+      { option: "4", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 5 x 2?",
+    answers: [
+      { option: "9", correct: false },
+      { option: "10", correct: true },
+      { option: "11", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 10 x 8?",
+    answers: [
+      { option: "800", correct: false },
+      { option: "90", correct: false },
+      { option: "80", correct: true },
+    ],
+  },
+  {
+    question: "Quanto é 7 x 7?",
+    answers: [
+      { option: "70", correct: false },
+      { option: "56", correct: false },
+      { option: "49", correct: true },
+    ],
+  },
+];
---
+++ script.js
@@ -0,0 +1,65 @@
+const question = document.querySelector(".question");
+const answers = document.querySelector(".answers");
+const spnQtd = document.querySelector(".spnQtd");
+const textFinish = document.querySelector(".finish span");
+const content = document.querySelector(".content");
+const contentFinish = document.querySelector(".finish");
+const btnRestart = document.querySelector(".finish button");
+
+import questions from "./questions.js";
+
+let currentIndex = 0;
+let questionsCorrect = 0;
+
+btnRestart.onclick = () => {
+  content.style.display = "flex";
+  contentFinish.style.display = "none";
+
+  currentIndex = 0;
+  questionsCorrect = 0;
+  loadQuestion();
+};
+
+function nextQuestion(e) {
+  if (e.target.getAttribute("data-correct") === "true") {
+    questionsCorrect++;
+  }
+
+  if (currentIndex < questions.length - 1) {
+    currentIndex++;
+    loadQuestion();
+  } else {
+    finish();
+  }
+}
+
+function finish() {
+  textFinish.innerHTML = `você acertou ${questionsCorrect} de ${questions.length}`;
+  content.style.display = "none";
+  contentFinish.style.display = "flex";
+}
+
+function loadQuestion() {
+  spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
+  const item = questions[currentIndex];
+  answers.innerHTML = "";
+  question.innerHTML = item.question;
+
+  item.answers.forEach((answer) => {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+    <button class="answer" data-correct="${answer.correct}">
+      ${answer.option}
+    </button>
+    `;
+
+    answers.appendChild(div);
+  });
+
+  document.querySelectorAll(".answer").forEach((item) => {
+    item.addEventListener("click", nextQuestion);
+  });
+}
+
+loadQuestion();
